71ca3e6a91d3ebde8dd23803c35b779fc56cdea3
nicklhy <nicklhy@gmail.com>
2015-07-05 10:38:04 +0800

    update src/caffe/train.py

--- src/caffe/train.py
+++ src/caffe/train.py
@@ -92,8 +92,8 @@ class ModelTrainer:
                 t2 = time.time()
                 print 'speed: {:.3f}s / iter'.format((t2-t1)/self.solver_param.display)
                 t1 = t2
-            # if self.solver.iter % (self.solver_param.test_interval) == 0:
-            if True:
+            if self.solver.iter % (self.solver_param.test_interval) == 0:
+            # if True:
                 print '#################### test begin ####################'
                 t5 = time.time()
                 test_num = len(self.test_gt)
