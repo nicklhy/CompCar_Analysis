276f47ec8547e924540b3f026c4b87df8a35491e
nicklhy <nicklhy@gmail.com>
2015-07-10 15:13:14 +0800

    update feature extraction part

--- src/caffe/extract_deep_feature.py
+++ src/caffe/extract_deep_feature.py
@@ -11,38 +11,38 @@ sys.path.insert(0, os.path.join(CAFFE_ROOT, 'python'))
 import caffe
 
 parser = argparse.ArgumentParser(description='Train a caffe model')
+parser.add_argument('--list_file', dest='list_file',
+                    help='image list file', type=str)
+parser.add_argument('--data_dir', dest='data_dir',
+                    help='DATA ROOT PATH',
+                    default='data/cropped_image', type=str)
+parser.add_argument('--output', dest='output',
+                    help='output ',
+                    default='', type=str)
+parser.add_argument('--model_def', dest='model_def',
+                    help='network definition file',
+                    default='models/bvlc_googlenet_deploy.prototxt', type=str)
+parser.add_argument('--weights', dest='weights',
+                    help='model weights file',
+                    default='models/bvlc_googlenet.caffemodel', type=str)
 parser.add_argument('--gpu_id', dest='gpu_id',
-                    help='GPU device to use [0]',
-                    default=0, type=int)
-parser.add_argument('--rank_num)', dest='rank_num',
-                    help='rank num',
-                    default=1, type=int)
-parser.add_argument('--phase', dest='phase',
-                    help='train test',
-                    default='train', type=str)
-parser.add_argument('--task', dest='task',
-                    help='all front rear side front_side rear_side',
-                    default='all', type=str)
-parser.add_argument('--level', dest='level',
-                    help='make model',
-                    default='make', type=str)
+                    help='GPU device to use [0]', default=0, type=int)
 args = parser.parse_args()
 
-if args.task == 'all':
-    task_str = ''
-else:
-    task_str = '_'+args.task
-level_str = '_'+args.level
-solver_prototxt = 'models/compcar'+task_str+level_str+'/solver.prototxt'
-pretrained_model = 'models/bvlc_googlenet.caffemodel'
+if args.list_file == '':
+    print 'please input a image list file'
+    sys.exit(-1)
 
 
-DATA_ROOT = 'data'
-im_list_file = 'data/train_test_split/classification/'+args.phase+task_str+'.txt'
+DATA_ROOT = args.data_dir
+im_list_file = args.list_file
 with open(im_list_file) as fd:
-    gt = map(lambda s: s.strip(), fd.readlines())
+    im_list = [os.path.join(DATA_ROOT, x) for x in map(lambda s: s.strip(), fd.readlines())]
 
-FEAT_FILE = './cache/%s_deep_feat.pkl' % args.phase
+if args.output=='':
+    FEAT_FILE = './cache/%s_deep_feat.pkl' % args.list_file
+else:
+    FEAT_FILE = args.output
 
 
 if os.path.exists(FEAT_FILE):
@@ -51,7 +51,7 @@ if os.path.exists(FEAT_FILE):
         print "feature file loaded from " + FEAT_FILE
 else:
     caffe.set_device(args.gpu_id)
-    net = caffe.Net(solver_prototxt, pretrained_model, caffe.TEST)
+    net = caffe.Net(args.model_def, args.weights, caffe.TEST)
     caffe.set_mode_gpu()
     transformer = caffe.io.Transformer({'data': net.blobs['data'].data.shape})
     transformer.set_transpose('data', (2, 0, 1))
@@ -60,16 +60,16 @@ else:
 
     net.blobs['data'].reshape(1, 3, 224, 224)
 
-    feats = np.zeros([len(gt), net.blobs['pool5/7x7_s1'].data.shape[1]])
+    feats = np.zeros([len(im_list), net.blobs['pool5/7x7_s1'].data.shape[1]])
     print 'feature extraction begins ... ...'
-    for i, img_path in enumerate(gt):
+    for i, img_path in enumerate(im_list):
         t1 = time.time()
-        net.blobs['data'].data[...] = transformer.preprocess('data', caffe.io.load_image(os.path.join(DATA_ROOT, 'cropped_image', img_path)))
+        net.blobs['data'].data[...] = transformer.preprocess('data', caffe.io.load_image(img_path))
         out = net.forward()
         feats[i, :] = net.blobs['pool5/7x7_s1'].data.flatten()
         t2 = time.time()
         print img_path+' finished in %f seconds' % (t2-t1)
     print 'feature extraction finished ... ...'
-    feats = ([x[0] for x in gt], feats)
+    feats = (im_list, feats)
     with open(FEAT_FILE, 'wb') as fd:
         cPickle.dump(feats, fd, cPickle.HIGHEST_PROTOCOL)
